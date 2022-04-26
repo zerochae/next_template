ae6c25c5da5209ab159cd64b5a6bcca7fdbb1ff3
zerochae <zerochae@kakao.com>
2022-04-26 18:59:52 +0900

    :lipstick:Design: change css

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,14 +2,23 @@ import styled from "@emotion/styled/macro";
 import { css } from "@emotion/react";
 import tw from "twin.macro";
 
-const MyButton = styled.button([
-  tw`
-bg-[red]`,
-  css`
-    font-size: 40px;
-    font-weight: bold;
-  `,
-]);
+// const MyButton = styled.button([
+//   tw`
+// bg-[red]`,
+//   css`
+//     font-size: 40px;
+//     font-weight: bold;
+//   `,
+// ]);
+
+const MyButton = styled.button`
+  ${tw`
+text-3xl
+font-bold
+underline
+`}
+  color: red;
+`;
 
 const Button = () => {
   return <MyButton>button</MyButton>;
