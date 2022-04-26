79a233549b077a27538be2fe8cc763d768c62778
zerochae <zerochae@kakao.com>
2022-04-26 18:13:56 +0900

    :lipstick:Design add tw macro css

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,5 +1,18 @@
+import styled from "@emotion/styled/macro";
+import { css } from "@emotion/react";
+import tw from "twin.macro";
+
+const MyButton = styled.button([
+  tw`
+bg-[red]`,
+  css`
+    font-size: 40px;
+    font-weight: bold;
+  `,
+]);
+
 const Button = () => {
-  return <button>button</button>;
+  return <MyButton>button</MyButton>;
 };
 
 export default Button;
